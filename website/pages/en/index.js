445124aa3ce5e75e5bc7dc58e7b1a844aa51d12a
Daniele <crtdaniele@gmail.com>
2020-07-22 12:56:57 +0200

    Create index.js

---
+++ website/pages/en/index.js
@@ -0,0 +1,19 @@
+/**
+ * Copyright (c) 2017-present, Facebook, Inc.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+const React = require('react');
+
+function Index() {
+  return (
+    <meta
+      http-equiv="refresh"
+      content="0; url=docs/home"
+    />
+  );
+}
+
+module.exports = Index;
