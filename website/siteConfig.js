f0273480708909b92817399d311676f088c1448a
Daniele <crtdaniele@gmail.com>
2020-07-22 10:27:14 +0200

    Update siteConfig.js

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -23,7 +23,7 @@ const users = [
 const siteConfig = {
   title: 'Daniele Carta\'s Docs', // Title for your website.
   tagline: 'A website for testing',
-  baseUrl: '/docs/home/', // Base URL for your project */
+  baseUrl: '/', // Base URL for your project */
   url: 'https://danielecarta-docs.netlify.app',
   // Used for publishing and more
   projectName: 'danielecarta-docs', 
